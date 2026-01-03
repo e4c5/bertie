6a635f9a658d427e7e3c086b02e7dfbdae8ebbd5
raditha dissanayake <raditha@raditha.com>
2026-01-03 16:35:35 +0530

    change pom to get dependencies from jitpack

--- src/main/java/com/raditha/dedup/analyzer/DuplicationAnalyzer.java
+++ src/main/java/com/raditha/dedup/analyzer/DuplicationAnalyzer.java
@@ -19,8 +19,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Main orchestrator for duplicate detection.
